c7fc63db65e3f833e421737d7db5c145566d8ee1
bluemoon0011 <xin.jin0011@gmail.com>
2018-10-04 18:29:43 -0400

    update valueIterationAgents.py

--- Project 3/valueIterationAgents.py
+++ Project 3/valueIterationAgents.py
@@ -26,7 +26,8 @@ class ValueIterationAgent(ValueEstimationAgent):
         discount factor.
     """
     def __init__(self, mdp, discount = 0.9, iterations = 100):
-        """
+        '''
+        desc:
           Your value iteration agent should take an mdp on
           construction, run the indicated number of iterations
           and then act according to the resulting policy.
@@ -37,7 +38,12 @@ class ValueIterationAgent(ValueEstimationAgent):
               mdp.getTransitionStatesAndProbs(state, action)
               mdp.getReward(state, action, nextState)
               mdp.isTerminal(state)
-        """
+
+        :param mdp: mdp class
+        :param discount: discount rate
+        :param iterations: number of iterations
+        '''
+
         self.mdp = mdp
         self.discount = discount
         self.iterations = iterations
@@ -54,22 +60,29 @@ class ValueIterationAgent(ValueEstimationAgent):
                     maxQValue=float("-inf") # initialize the maxValue as -inf.
                     for action in self.mdp.getPossibleActions(currentState): # check all the possible actions that current state can take.
                         nextQValue=self.computeQValueFromValues(currentState,action) # get the max{maxValue, nextQValue}
-                        maxQValue=max(nextQValue,maxQValue)# update max Q value
+                        maxQValue=max(nextQValue,maxQValue)# update max Q-value
                     nextValues[currentState]=maxQValue
             self.values=nextValues
 
     def getValue(self, state):
-        """
-          Return the value of the state (computed in __init__).
-        """
+        '''
+        desc:  get the value of current state
+        :param state: current state
+        :return: the value of the state (computed in __init__).
+        '''
+
         return self.values[state]
 
 
     def computeQValueFromValues(self, state, action):
-        """
+        '''
+        desc:
           Compute the Q-value of action in state from the
           value function stored in self.values.
-        """
+        :param state:
+        :param action:
+        :return:
+        '''
         "*** YOUR CODE HERE ***"
 
         QValue=0
@@ -81,14 +94,17 @@ class ValueIterationAgent(ValueEstimationAgent):
         return QValue
 
     def computeActionFromValues(self, state):
-        """
+        '''
+        desc:
           The policy is the best action in the given state
           according to the values currently stored in self.values.
 
           You may break ties any way you see fit.  Note that if
           there are no legal actions, which is the case at the
           terminal state, you should return None.
-        """
+        :param state: current state
+        :return: the policy (optimal action) for current state
+        '''
         "*** YOUR CODE HERE ***"
         QValues= util.Counter()
         policy = None # if state is the terminal state, we won't change policy and this function will return None for policy
@@ -100,15 +116,25 @@ class ValueIterationAgent(ValueEstimationAgent):
 
     def getPolicy(self, state):
         '''
-          desc: get the policy (optimal action) you want
+        desc: get the policy (optimal action) you want
         :param state: the current state
         :return: the policy that current state should take
         '''
         return self.computeActionFromValues(state)
 
     def getAction(self, state):
-        "Returns the policy at the state (no exploration)."
+        '''
+        desc: Returns the policy at the state (no exploration).
+        :param state: current state
+        :return: the optimal action for current state
+        '''
         return self.computeActionFromValues(state)
 
     def getQValue(self, state, action):
+        '''
+        desc: calculate the Q-value for current state and action
+        :param state: current state
+        :param action: the action that you will take
+        :return: Q-value
+        '''
         return self.computeQValueFromValues(state, action)
